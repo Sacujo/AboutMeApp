0bbcf7c1be91e4b51478dfdbf71fd8d6926bd425
Igor Guryan <sacujo93@yandex.ru>
2024-09-19 00:24:30 +0300

    fix methods

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -15,38 +15,37 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loginTextField.delegate = self
-        passwordTextField.delegate = self
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
     }
     
     @IBAction func forgotLoginButtonTapped(_ sender: UIButton) {
-        showAlert(title: "Forgot login?", message: "Your login is \(login)")
+        showAlert(title: "Forgot login?", message: "Your login is \(login)") {
+            self.loginTextField.becomeFirstResponder()
+        }
     }
     
     @IBAction func forgotPassButtonTapped(_ sender: UIButton) {
-        showAlert(title: "Forgot password?", message: "Your password is \(password)")
+        showAlert(title: "Forgot password?", message: "Your password is \(password)") {
+            self.passwordTextField.text = ""
+            self.passwordTextField.becomeFirstResponder()
+        }
     }
     
-    private func showAlert(title: String, message: String) {
+    private func showAlert(title: String, message: String, completion: (() -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default) { _ in
-            self.clearTextFields()
-            self.loginTextField.becomeFirstResponder()
+            completion?()
         }
         alert.addAction(action)
         present(alert, animated: true)
-    }
-    
-    private func clearTextFields() {
-        loginTextField.text = ""
-        passwordTextField.text = ""
     }
 }
 
 //MARK: - Sugue Methods
 extension LoginViewController {
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         guard loginTextField.text == login, passwordTextField.text == password else {
             showAlert(title: "Wrong login or password", message: "Please, enter correct login and password")
@@ -61,29 +60,11 @@ extension LoginViewController {
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        clearTextFields()
+        loginTextField.text = ""
+        passwordTextField.text = ""
     }
 }
-
-//MARK: - TextField Methods
-extension LoginViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        switch textField {
-        case loginTextField:
-            passwordTextField.becomeFirstResponder()
-        case passwordTextField:
-            passwordTextField.resignFirstResponder()
-            // Здесь нужно добавить то же действие что и при нажатии Log In Button
-        default:
-            break
-        }
-        return true
-    }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        self.view.endEditing(true)
-    }
-}
+    
 
 
